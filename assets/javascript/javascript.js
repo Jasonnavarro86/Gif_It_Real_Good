f608e7138bd2aab3969552cb692e46bed2b40ade
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-09-03 09:50:31 -0400

    added instructions for the user at the load of the page.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,31 +1,45 @@
+// My object for the game.
 var giphyMagic = {
-// This is the array holding all the movie buttons.
-    myMovies: ["moana", "frozen", "tangled", "the lion king" ,"mulan", "beauty and the beast", "the little mermaid", "the jungle book"],
-    // This function() creates the movie buttons and is called everytime a new movie is added. 
+    // Property 1: This is the array holding all the movie buttons.
+    myMovies: ["moana", "frozen", "tangled", "the lion king", "mulan", "beauty and the beast", "the little mermaid", "the jungle book"],
+
+    // Property 2: This function() creates the movie buttons and is called everytime a new movie is added. 
     makeButtons: function () {
+
         $(".buttonsHere").empty();
+
         for (var i = 0; i < this.myMovies.length; i++) {
+
             $('<button>').addClass("btn jsonInfo")
                 .html(this.myMovies[i]).appendTo(".buttonsHere")
         }
     },
-    // This function() pushes the users movie choice into the movie array and empties the input box. 
+    // Property 3: This function() pushes the users movie choice into the movie array and empties the input box. 
     pushIntoArray: function () {
+
         $(".searchButton").on("click", function () {
+            $(".hideMe").hide()
             event.preventDefault();
             var userInput = $(".gifInput").val();
+
             giphyMagic.myMovies.push(userInput)
             $(".gifInput").val("");
+
             giphyMagic.makeButtons();
             giphyMagic.myMovies2();
         })
     },
-    // This function() calls the giphy API and puts the GIF's on the page when the movie buttons are clicked. 
+    // Property 4: This function() calls the giphy API and puts the GIF's on the page when the movie buttons are clicked. 
     myMovies2: function () {
+
         $(".jsonInfo").on("click", function () {
+            $(".hideMe").hide()
             $(".gifsHere").empty();
+
             var newMovie = $(this).html();
+
             var movieUrl = "https://api.giphy.com/v1/gifs/search?q=" + newMovie + "&api_key=dc75eaa47ff2402cb92349f140f9aab2&limit=10";
+
             $.ajax({
                 url: movieUrl,
                 method: "GET"

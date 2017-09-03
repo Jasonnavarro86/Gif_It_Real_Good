83b06c1bb13ba696cb1e0735948520417fd59f2b
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-09-02 22:31:35 -0400

    I put all my functions in an object

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,68 +1,57 @@
-var myMovies = ["aladdin", "lion king", "mulan", "beauty and the beast", "moana", "toy story", "the jungle book"];
+var giphyMagic = {
 
-function makeButtons() {
-    $(".buttonsHere").empty();
-
-    for (var i = 0; i < myMovies.length; i++) {
-
-        $('<button>').addClass("btn jsonInfo")
-            .html(myMovies[i]).appendTo(".buttonsHere")
-    }
-}
-
-$(".searchButton").on("click", function () {
-    event.preventDefault();
-
-    var userInput = $(".gifInput").val();
-
-    myMovies.push(userInput);
-    $(".gifInput").val("");
-
-    makeButtons();
-    myMovies2();
-})
-makeButtons();
-
-
-function myMovies2() {
-    $(".jsonInfo").on("click", function () {
-        $(".gifsHere").empty();
-        var newMovie = $(this).html();
-        var movieUrl = "https://api.giphy.com/v1/gifs/search?q=" + newMovie + "&api_key=dc75eaa47ff2402cb92349f140f9aab2&limit=10"
-
-        $.ajax({
-            url: movieUrl,
-            method: "GET"
-        }).done(function (result) {
-
-            var jsonGif = result
-            console.log(jsonGif);
-            for (var i = 0; i < result.data.length; i++) {
-
-                var newImg = $("<img>")
-                newImg.addClass("col btn gifAnimate")
-                newImg.attr("src", result.data[i].images.fixed_height_still.url)
-                newImg.attr("data-status", "still")
-                newImg.attr("data-animate", result.data[i].images.fixed_height.url)
-                newImg.attr("data-still", result.data[i].images.fixed_height_still.url).on("click", function () {
-
-                    console.log($(this).data());
-                    if ($(this).data("status") === "still") {
-                        console.log("hello");
-                        $(this).data("status", "moving")
-                        $(this).attr("src", $(this).data("animate"))
-                    } else {
-                        console.log('moving');
-                        $(this).data("status", "still")
-                        $(this).attr("src", $(this).data("still"))
-                    }
-                })
-
-
-                newImg.appendTo(".gifsHere")
-            }
+    myMovies: ["aladdin", "lion king", "mulan", "beauty and the beast", "moana", "toy story", "the jungle book"],
+    makeButtons: function () {
+        $(".buttonsHere").empty();
+        for (var i = 0; i < this.myMovies.length; i++) {
+            $('<button>').addClass("btn jsonInfo")
+                .html(this.myMovies[i]).appendTo(".buttonsHere")
+        }
+    },
+    pushIntoArray: function () {
+        $(".searchButton").on("click", function () {
+            event.preventDefault();
+            var userInput = $(".gifInput").val();
+            giphyMagic.myMovies.push(userInput)
+            $(".gifInput").val("");
+            giphyMagic.makeButtons();
+            giphyMagic.myMovies2();
         })
+    },
+    myMovies2: function () {
+        $(".jsonInfo").on("click", function () {
+            $(".gifsHere").empty();
+            var newMovie = $(this).html();
+            var movieUrl = "https://api.giphy.com/v1/gifs/search?q=" + newMovie + "&api_key=dc75eaa47ff2402cb92349f140f9aab2&limit=10";
+            $.ajax({
+                url: movieUrl,
+                method: "GET"
+            }).done(function (result) {
 
-    })
+                var jsonGif = result
+                for (var i = 0; i < result.data.length; i++) {
+
+                    var newImg = $("<img>")
+                    newImg.addClass("col btn gifAnimate")
+                    newImg.attr("src", result.data[i].images.fixed_height_still.url)
+                    newImg.attr("data-status", "still")
+                    newImg.attr("data-animate", result.data[i].images.fixed_height.url)
+                    newImg.attr("data-still", result.data[i].images.fixed_height_still.url).on("click", function () {
+
+                        if ($(this).data("status") === "still") {
+                            $(this).data("status", "moving")
+                            $(this).attr("src", $(this).data("animate"))
+                        } else {
+                            $(this).data("status", "still")
+                            $(this).attr("src", $(this).data("still"))
+                        }
+                    })
+                    newImg.appendTo(".gifsHere");
+                }
+            })
+        })
+    },
 }
-myMovies2();
+giphyMagic.pushIntoArray();
+giphyMagic.makeButtons();
+giphyMagic.myMovies2();
